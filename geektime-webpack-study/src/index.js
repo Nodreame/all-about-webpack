8ddd952480a0ce212fd820c7dc19982cb9eeae6f
Nodreame <nodreame@163.com>
2020-01-13 22:21:36 +0800

    feat(geektime-webpack-study): pref es6 & react config

--- geektime-webpack-study/src/index.js
+++ geektime-webpack-study/src/index.js
@@ -1,3 +1,5 @@
 import { helloworld } from './helloworld';
 
+[1, 2, 3].map(n => n + 1);
+
 document.write(helloworld());
--- geektime-webpack-study/webpack.config.js
+++ geektime-webpack-study/webpack.config.js
@@ -14,8 +14,17 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /.js$/,
-        use: 'babel-loader'
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              '@babel/preset-env',
+              '@babel/preset-react'
+            ]
+          }
+        }
       }
     ]
   },
